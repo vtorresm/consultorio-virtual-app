c794f59de4cb4d2d9e38b85255bd64b6074ddc97
Victor <vatorresm88@gmail.com>
2020-08-28 11:32:24 -0500

    Agregando vista para crear Cliente

---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+})
+export class FormComponent implements OnInit {
+  public cliente: Cliente = new Cliente();
+  titulo = 'Crear Cliente';
+
+  errores: string[];
+
+  constructor(
+    private clienteService: ClienteService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.cargarCliente();
+  }
+
+  cargarCliente(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      const id = params['id'];
+      if (id) {
+        this.clienteService
+          .getCliente(id)
+          .subscribe((cliente) => (this.cliente = cliente));
+      }
+    });
+  }
+
+  create(): void {
+    this.clienteService.create(this.cliente).subscribe(
+      (cliente) => {
+        this.router.navigate(['/clientes']);
+        swal.fire(
+          'Nuevo cliente',
+          `El cliente ${cliente.nombre} ha sido creado con éxito`,
+          'success'
+        );
+      },
+      (err) => {
+        this.errores = err.error.errors as string[];
+        console.error('Código del error desde el backend: ' + err.status);
+        console.error(err.error.errors);
+      }
+    );
+  }
+
+  update(): void {
+    this.clienteService.update(this.cliente).subscribe(
+      (json) => {
+        this.router.navigate(['/clientes']);
+        swal.fire(
+          'Cliente Actualizado',
+          `${json.mensaje}: ${json.cliente.nombre}`,
+          'success'
+        );
+      },
+      (err) => {
+        this.errores = err.error.errors as string[];
+        console.error('Código del error desde el backend: ' + err.status);
+        console.error(err.error.errors);
+      }
+    );
+  }
+}

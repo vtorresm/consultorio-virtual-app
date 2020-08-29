3043ca4cccc6d09c1219a4caf6c118812b4beab9
Victor Alberto Torres Mori <vtorremo@everis.com>
2020-08-28 21:49:47 -0500

    Actualizando header

--- src/app/pacientes/paciente.ts
+++ src/app/pacientes/paciente.ts
@@ -2,7 +2,14 @@ export class Paciente {
   id: number;
   nombre: string;
   apellido: string;
-  createAt: string;
+  dni: string;
+  genero: string;
+  fechaNacimiento: Date;
   email: string;
-
+  celular: string;
+  distrito: string;
+  direccion: string;
+  referencia: string;
+  interior: string;
+  createAt: string;
 }

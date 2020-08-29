b4ae1242e168791d429796d582cb5fe88ac54d30
Victor <vatorresm88@gmail.com>
2020-08-29 17:27:20 -0500

    Agregando datos a la vista paciente

--- src/app/components/pacientes/form.component.ts
+++ src/app/components/pacientes/form.component.ts
@@ -6,57 +6,67 @@ import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
-  templateUrl: './form.component.html'
+  templateUrl: './form.component.html',
 })
 export class FormComponent implements OnInit {
-
   public paciente: Paciente = new Paciente();
   titulo = 'Crear Paciente';
 
   errores: string[];
 
-  constructor(private pacienteService: PacienteService,
-              private router: Router,
-              private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private pacienteService: PacienteService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   // tslint:disable-next-line: typedef
   ngOnInit() {
-    this.activatedRoute.paramMap.subscribe(params => {
+    this.activatedRoute.paramMap.subscribe((params) => {
       const id = +params.get('id');
       if (id) {
-        this.pacienteService.getPaciente(id).subscribe((paciente) => this.paciente = paciente);
+        this.pacienteService
+          .getPaciente(id)
+          .subscribe((paciente) => (this.paciente = paciente));
       }
     });
   }
 
   create(): void {
-    this.pacienteService.create(this.paciente)
-      .subscribe(
-        paciente => {
-          this.router.navigate(['/pacientes']);
-          swal.fire('Nuevo paciente', `El paciente ${paciente.nombre} ha sido creado con éxito`, 'success');
-        },
-        err => {
-          this.errores = err.error.errors as string[];
-          console.error('Código del error desde el backend: ' + err.status);
-          console.error(err.error.errors);
-        }
-      );
+    console.log(this.paciente);
+    this.pacienteService.create(this.paciente).subscribe(
+      (paciente) => {
+        this.router.navigate(['/pacientes']);
+        swal.fire(
+          'Nuevo paciente',
+          `El paciente ${paciente.nombre} ha sido creado con éxito`,
+          'success'
+        );
+      },
+      (err) => {
+        this.errores = err.error.errors as string[];
+        console.error('Código del error desde el backend: ' + err.status);
+        console.error(err.error.errors);
+      }
+    );
   }
 
   update(): void {
-    this.pacienteService.update(this.paciente)
-      .subscribe(
-        json => {
-          this.router.navigate(['/pacientes']);
-          swal.fire('Paciente Actualizado', `${json.mensaje}: ${json.Paciente.nombre}`, 'success');
-        },
-        err => {
-          this.errores = err.error.errors as string[];
-          console.error('Código del error desde el backend: ' + err.status);
-          console.error(err.error.errors);
-        }
-      );
+    console.log(this.paciente);
+    this.pacienteService.update(this.paciente).subscribe(
+      (json) => {
+        this.router.navigate(['/pacientes']);
+        swal.fire(
+          'Paciente Actualizado',
+          `${json.mensaje}: ${json.Paciente.nombre}`,
+          'success'
+        );
+      },
+      (err) => {
+        this.errores = err.error.errors as string[];
+        console.error('Código del error desde el backend: ' + err.status);
+        console.error(err.error.errors);
+      }
+    );
   }
-
 }
--- src/app/components/pacientes/paciente.service.ts
+++ src/app/components/pacientes/paciente.service.ts
@@ -1,91 +1,93 @@
 import { Injectable } from '@angular/core';
 import { Paciente } from './paciente';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { map, catchError, tap } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
-import swal from 'sweetalert2';
 import { Router } from '@angular/router';
-
 
 @Injectable()
 export class PacienteService {
   private urlEndPoint = 'http://localhost:8080/api/pacientes';
 
-  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
-
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   getPacientes(page: number): Observable<any> {
     return this.http.get(this.urlEndPoint + '/page/' + page).pipe(
       tap((response: any) => {
         console.log('PacienteService: tap 1');
-        (response.content as Paciente[]).forEach(paciente => console.log(paciente.nombre));
+        (response.content as Paciente[]).forEach((paciente) =>
+          console.log(paciente.nombre)
+        );
       }),
       map((response: any) => {
-        (response.content as Paciente[]).map(paciente => {
-          paciente.nombre = paciente.nombre.toUpperCase();
+        (response.content as Paciente[]).map((paciente) => {
+          paciente.nombre = paciente.nombre;
           return paciente;
         });
         return response;
       }),
-      tap(response => {
+      tap((response) => {
         console.log('PacienteService: tap 2');
-        (response.content as Paciente[]).forEach(paciente => console.log(paciente.nombre));
+        (response.content as Paciente[]).forEach((paciente) =>
+          console.log(paciente.nombre)
+        );
       })
     );
   }
 
   create(paciente: Paciente): Observable<Paciente> {
-    return this.http.post(this.urlEndPoint, paciente, { headers: this.httpHeaders })
-      .pipe(
-        map((response: any) => response.paciente as Paciente),
-        catchError(e => {
-
-          if (e.status === 400) {
-            return throwError(e);
-          }
-
-          console.error(e.error.mensaje);
-          swal.fire(e.error.mensaje, e.error.error, 'error');
+    return this.http.post(this.urlEndPoint, paciente).pipe(
+      map((response: any) => response.paciente as Paciente),
+      catchError((e) => {
+        if (e.status === 400) {
           return throwError(e);
-        })
-      );
+        }
+
+        if (e.error.mensaje) {
+          console.error(e.error.mensaje);
+        }
+
+        return throwError(e);
+      })
+    );
   }
 
   getPaciente(id): Observable<Paciente> {
     return this.http.get<Paciente>(`${this.urlEndPoint}/${id}`).pipe(
-      catchError(e => {
-        this.router.navigate(['/pacientes']);
-        console.error(e.error.mensaje);
-        swal.fire('Error al editar', e.error.mensaje, 'error');
+      catchError((e) => {
+        if (e.status !== 401 && e.error.mensaje) {
+          this.router.navigate(['/pacientes']);
+          console.error(e.error.mensaje);
+        }
         return throwError(e);
       })
     );
   }
 
   update(paciente: Paciente): Observable<any> {
-    return this.http.put<any>(`${this.urlEndPoint}/${paciente.id}`, paciente, { headers: this.httpHeaders }).pipe(
-      catchError(e => {
-
-        if (e.status === 400) {
+    return this.http
+      .put<any>(`${this.urlEndPoint}/${paciente.id}`, paciente)
+      .pipe(
+        catchError((e) => {
+          if (e.status === 400) {
+            return throwError(e);
+          }
+          if (e.error.mensaje) {
+            console.error(e.error.mensaje);
+          }
           return throwError(e);
-        }
-
-        console.error(e.error.mensaje);
-        swal.fire(e.error.mensaje, e.error.error, 'error');
-        return throwError(e);
-      })
-    );
+        })
+      );
   }
 
   delete(id: number): Observable<Paciente> {
-    return this.http.delete<Paciente>(`${this.urlEndPoint}/${id}`, { headers: this.httpHeaders }).pipe(
-      catchError(e => {
-        console.error(e.error.mensaje);
-        swal.fire(e.error.mensaje, e.error.error, 'error');
+    return this.http.delete<Paciente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError((e) => {
+        if (e.error.mensaje) {
+          console.error(e.error.mensaje);
+        }
         return throwError(e);
       })
     );
   }
-
 }

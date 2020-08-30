4519ae91ffef843a5da137ff43c98a2fb71a3cd2
Victor Alberto Torres Mori <vtorremo@everis.com>
2020-08-29 22:32:45 -0500

    Estilizando el Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,6 +8,7 @@ import swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
   titulo = 'Por favor Sign In!';
